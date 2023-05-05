fe695e05d7db45a855547fd760fd48fec2bde994
ALAN CRAMER <alancharlescramer@gmail.com>
2023-05-04 19:51:18 -0600

    first schema and fake dev data

---
+++ schemas/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE sensors (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  type_id INTEGER,
+  mac_address TEXT UNIQUE,
+  FOREIGN KEY (type_id) REFERENCES sensor_types(id)
+);
+
+CREATE TABLE sensor_types (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE readings (
+  id INTEGER PRIMARY KEY,
+  sensor_id INTEGER,
+  timestamp INTEGER,
+  data_type TEXT,   -- the name of the table with the data
+  data_id INTEGER,  -- the row of the table
+  FOREIGN KEY (sensor_id) REFERENCES sensors(id)
+);
+
+CREATE TABLE image_data (
+  id INTEGER PRIMARY KEY,
+  filename TEXT,
+  timestamp INTEGER,
+  FOREIGN KEY (id) REFERENCES readings(data_id)
+);
+
+CREATE TABLE motion_events (
+  id INTEGER PRIMARY KEY,
+  motion_detected INTEGER,  -- I'm thinking, "motion changed"
+  timestamp INTEGER,
+  FOREIGN KEY (id) REFERENCES readings(data_id)
+);
