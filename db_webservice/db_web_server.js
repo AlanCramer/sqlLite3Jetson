f08173841d77dc26be7479c9cd468a70db4e7156
ALAN CRAMER <alancharlescramer@gmail.com>
2023-06-09 10:37:08 -0600

    various improvements

--- db_webservice/db_web_server.js
+++ db_webservice/db_web_server.js
@@ -6,14 +6,19 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+
 const port = 3001;
 const secret = 'mysecretkey';
 
 // Open the SQLite database
 const db = new sqlite3.Database('dev.db');
 
-// Middleware to parse JSON request bodies
-app.use(bodyParser.json());
+// this ain't right, should be read from the detabase
+// there is a table with the event types (called data_types)
+const IMAGE_EVENT = 1;
+const MOTION_EVENT = 2;
+
 
 // Middleware to check JWT token and set req.user
 function authenticateToken(req, res, next) {
@@ -39,6 +44,20 @@ function authenticateToken(req, res, next) {
 app.get('/api/sensors', authenticateToken, (req, res) => {
   // Query the database for sensor data
   db.all('SELECT * FROM sensors', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      // Return the data as JSON
+      res.json(rows);
+    }
+  });
+});
+
+// Define a route for getting sensor data
+app.get('/api/data_types', authenticateToken, (req, res) => {
+  // Query the database for sensor data
+  db.all('SELECT * FROM data_types', (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Internal server error' });
@@ -92,9 +111,11 @@ app.get('/api/image-data', authenticateToken, (req, res) => {
 });
 
 // Define a route for getting motion event data
-app.get('/api/motion-events', authenticateToken, (req, res) => {
+app.get('/api/motion-readings', authenticateToken, (req, res) => {
+
+  console.log("got a motion-readings request");
   // Query the database for motion event data
-  db.all('SELECT * FROM motion_events', (err, rows) => {
+  db.all('SELECT * FROM motion_data', (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Internal server error' });
@@ -126,7 +147,51 @@ app.get('/api/people-detected', authenticateToken, (req, res) => {
   });
 
 
+// Writing apis from sensors
+
+
+app.post('/motion-reading', (req, res) => {
+  const { sensorId, motion, time_read } = req.body;
+  const MOTION_EVENT_ID = 2;
+
+  console.log("got a motion-reading request");
+
+  db.serialize(() => {
+    db.run('PRAGMA foreign_keys = ON;');
+    db.run('BEGIN TRANSACTION');
+
+    // Step 1: Add to motion_data table
+    let motionQuery = 'INSERT INTO motion_data(motion_detected) VALUES (?)';
+    db.run(motionQuery, [motion], function (err) {
+      if (err) {
+        console.log(" error in the motion query");
+        return res.status(500).json({ error: err.message });
+      }
+      console.log(` added motion query, row: ${this.lastID}`);
+
+      let motionId = this.lastID;
+
+      // Step 2: Add to readings table
+      let readingsData = [null, sensorId, time_read, Date.now(), MOTION_EVENT_ID, motionId];
+      let readingsQuery = 'INSERT INTO readings VALUES (?, ?, ?, ?, ?, ?)';
+      db.run(readingsQuery, readingsData, (err) => {
+        if (err) {
+          db.run('ROLLBACK');
+          console.log(" error with reading, rolling back");
+          return res.status(500).json({ error: err.message });
+        }
+
+        db.run('COMMIT');
+        console.log(" added motion event");
+        return res.status(200).json({ message: 'Data added' });
+      });
+    });
+  });
+});
+
+
+
 // Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on network port ${port}`);
 });
